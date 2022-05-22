f3029c1bff442eaff43d298a198d28ed6b9bc177
Pavel <pavel-zh@inbox.ru>
2022-05-22 11:37:32 +0300

    add files for messenger

---
+++ common/variables.py
@@ -0,0 +1,25 @@
+"""
+Константы проекта
+"""
+
+# Порт по умолчанию для сетевого взаимодействия
+DEFAULT_PORT = 7777
+# IP адрес по умолчанию для подключения клиента
+DEFAULT_IP_address = '127.0.0.1'
+# Максималная очередь подключений
+MAX_CONNECTIONS = 5
+# Максимальная длина сообщения в байтах
+MAX_PACKAGE_LENGTH = 1024
+# Кодировка проекта
+ENCODING = 'utf-8'
+
+# Описание протокола JIM:
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+# Прочие ключи, используемые в протоколе
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'

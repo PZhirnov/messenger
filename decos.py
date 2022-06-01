6c3835af568f344fa6fc5f45fcc1d0ce0b8e5799
Pavel <pavel-zh@inbox.ru>
2022-06-01 13:57:17 +0300

    coorect decos

--- decos.py
+++ decos.py
@@ -1,9 +1,8 @@
 import inspect
 import sys
 import logging
-import traceback
-
 import logs.server_log_config
+import traceback
 
 
 def log(func_to_log):
@@ -12,12 +11,16 @@ def log(func_to_log):
         logger_name = 'server' if 'server.py' in sys.argv[0] else 'client'
         LOGGER = logging.getLogger(logger_name)
         rt = func_to_log(*args, *kwargs)
+
+        # к условию по п.1
         LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} с параметрами {args}, {kwargs}')
+        # к условию по п.2
+        LOGGER.debug(f'Функция {func_to_log.__name__} вызвана '
+                     f'из функции {traceback.format_stack()[0].strip().split()[-1]}')
+
+        # LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} с параметрами {args}, {kwargs}'
+        #              f'Вызов из модуля {func_to_log.__module__}'
+        #              f'Вызов из функции {traceback.format_stack()[0].strip().split()[-1]}')
 
         return rt
     return log_saver
-
-
-print(sys.argv[0])
-print(traceback.format_stack())
-print(inspect.stack()[0].code_context)

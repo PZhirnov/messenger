d7d19858d18ced16d2c025a7c37009a3be398732
Pavel <pavel-zh@inbox.ru>
2022-08-06 15:52:26 +0300

    modified launcher

--- launcher.py
+++ launcher.py
@@ -5,22 +5,19 @@ import subprocess
 PROCESSES = []
 
 while True:
-    ACTION = input('Выберите действие: q - выход, '
+    ACTION: str = input('Выберите действие: q - выход, '
                    's - запустить сервер и клиенты, '
                    'x - закрыть все окна: ')
 
     if ACTION == 'q':
         break
     elif ACTION == 's':
+        clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
         PROCESSES.append(subprocess.Popen('python server.py',
                                           creationflags=subprocess.CREATE_NEW_CONSOLE))
-        PROCESSES.append(subprocess.Popen('python client.py -n test1',
-                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
-        PROCESSES.append(subprocess.Popen('python client.py -n test2',
-                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
-        PROCESSES.append(subprocess.Popen('python client.py -n test3',
-                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
+        for i in range(clients_count):
+            PROCESSES.append(subprocess.Popen(f'python client.py -n test{i+1}', creationflags=subprocess.CREATE_NEW_CONSOLE))
+
     elif ACTION == 'x':
         while PROCESSES:
-            VICTIM = PROCESSES.pop()
-            VICTIM.kill()
+            PROCESSES.pop().kill()

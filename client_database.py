004096958e06a8f14b74dcf2f9b2d441e43fff03
Pavel <pavel-zh@inbox.ru>
2022-08-13 11:46:37 +0300

    add methods

--- client_database.py
+++ client_database.py
@@ -106,6 +106,34 @@ class ClientDatabase:
     def get_contacts(self):
         return [contact[0] for contact in self.session.query(self.Contacts.name).all()]
 
+    # Функция возвращает список известных пользователей
+    def get_users(self):
+        return [user[0] for user in self.session.query(self.KnownUsers.username).all()]
+
+    # Функция проверяет наличие пользователя в таблице Известных Пользователей
+    def check_user(self, user):
+        if self.session.query(self.KnownUsers).filter_by(username=user).count():
+            return True
+        else:
+            return False
+
+    # Функция проверяет наличие пользователя в таблице Контактов
+    def check_contact(self, contact):
+        if self.session.query(self.Contacts).filter_by(name=contact).count():
+            return True
+        else:
+            return False
+
+    # Функция возвращает историю переписки
+    def get_history(self, from_who=None, to_who=None):
+        query = self.session.query(self.MessageHistory)
+        if from_who:
+            query = query.filter_by(from_user=from_who)
+        if to_who:
+            query = query.filter_by(to_user=to_who)
+        return [(history_row.from_user, history_row.to_user, history_row.message, history_row.date)
+                for history_row in query.all()]
+
 
 
 

fe3d141b4d5fc00463f16e02a4091908eee5ef7b
Pavel <pavel-zh@inbox.ru>
2022-08-17 09:26:43 +0300

    correct launcher windows

--- launcher.py
+++ launcher.py
@@ -7,15 +7,19 @@ PROCESSES = []
 
 while True:
     ACTION: str = input('Выберите действие: q - выход, '
-                   's - запустить сервер и клиенты, '
+                   's - запустить сервер и клиенты, k - запустить клиенты, '
                    'x - закрыть все окна: ')
 
     if ACTION == 'q':
         break
+
     elif ACTION == 's':
-        clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
+        # Запускаем сервер
         PROCESSES.append(subprocess.Popen('python server.py',
                                           creationflags=subprocess.CREATE_NEW_CONSOLE))
+    elif ACTION == 'k':
+        # Запускаем нужное количество клиентов
+        clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
         for i in range(clients_count):
             PROCESSES.append(subprocess.Popen(f'python client.py -n test{i+1}',
                                               creationflags=subprocess.CREATE_NEW_CONSOLE))

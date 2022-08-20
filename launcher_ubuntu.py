db6aaba34a0188e7ad9cc1e2d1f1c45accad70e8
Pavel <pavel-zh@inbox.ru>
2022-08-20 10:03:36 +0300

    correct launcher linux

--- launcher_ubuntu.py
+++ launcher_ubuntu.py
@@ -1,5 +1,5 @@
 """
-It is a launcher for starting subprocesses for server and clients of two types: senders and listeners.
+It is a launcher for starting subprocesses for server_dist and clients of two types: senders and listeners.
 for more information:
 https://stackoverflow.com/questions/67348716/kill-process-do-not-kill-the-subprocess-and-do-not-close-a-terminal-window
 """
@@ -16,7 +16,7 @@ BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_subprocess(file_with_args):
-    sleep(0.5)
+    sleep(1)
     file_full_path = f"{PYTHON_PATH} {BASE_PATH}/{file_with_args}"
     args = ["gnome-terminal", "--disable-factory", "--", "bash", "-c", file_full_path]
     return subprocess.Popen(args, preexec_fn=os.setpgrp)
@@ -24,7 +24,7 @@ def get_subprocess(file_with_args):
 
 process = []
 while True:
-    TEXT_FOR_INPUT = "Выберите действие: q - выход, s - запустить сервер и клиенты, x - закрыть все окна: "
+    TEXT_FOR_INPUT = "Выберите действие: q - выход , s - запустить сервер, k - запустить клиенты x - закрыть все окна: "
     action = input(TEXT_FOR_INPUT)
 
     if action == "q":
@@ -32,8 +32,14 @@ while True:
     elif action == "s":
         process.append(get_subprocess("server.py"))
 
-        for i in range(2):
-            process.append(get_subprocess(f"client.py -n test{i+1}"))
+    elif action == 'k':
+        print('Убедитесь, что на сервере зарегистрировано необходимо количество клиентов с паролем 123456.')
+        print('Первый запуск может быть достаточно долгим из-за генерации ключей!')
+        clients_count = int(
+            input('Введите количество тестовых клиентов для запуска: '))
+        # Запускаем клиентов:
+        for i in range(clients_count):
+            process.append(get_subprocess(f"client.py -n test{i + 1} -p 123456"))
 
     elif action == "x":
         while process:

371f9b49b93e6b7c193e1e1ad4d52e5fa0b330fb
Pavel <pavel-zh@inbox.ru>
2022-08-20 10:00:09 +0300

    correct launcher win

--- launcher.py
+++ launcher.py
@@ -3,27 +3,33 @@
 
 import subprocess
 
-PROCESSES = []
 
-while True:
-    ACTION: str = input('Выберите действие: q - выход, '
-                   's - запустить сервер и клиенты, k - запустить клиенты, '
-                   'x - закрыть все окна: ')
+def main():
+    PROCESSES = []
 
-    if ACTION == 'q':
-        break
+    while True:
+        ACTION: str = input('Выберите действие: q - выход, '
+                       's - запустить сервер и клиенты, k - запустить клиенты, '
+                       'x - закрыть все окна: ')
 
-    elif ACTION == 's':
-        # Запускаем сервер
-        PROCESSES.append(subprocess.Popen('python server.py',
-                                          creationflags=subprocess.CREATE_NEW_CONSOLE))
-    elif ACTION == 'k':
-        # Запускаем нужное количество клиентов
-        clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
-        for i in range(clients_count):
-            PROCESSES.append(subprocess.Popen(f'python client.py -n test{i+1}',
+        if ACTION == 'q':
+            break
+
+        elif ACTION == 's':
+            # Запускаем сервер
+            PROCESSES.append(subprocess.Popen('python server.py',
                                               creationflags=subprocess.CREATE_NEW_CONSOLE))
+        elif ACTION == 'k':
+            # Запускаем нужное количество клиентов
+            clients_count = int(input('Введите количество тестовых клиентов для запуска: '))
+            for i in range(clients_count):
+                PROCESSES.append(subprocess.Popen(f'python client.py -n test{i+1} -p 123456',
+                                                  creationflags=subprocess.CREATE_NEW_CONSOLE))
 
-    elif ACTION == 'x':
-        while PROCESSES:
-            PROCESSES.pop().kill()
+        elif ACTION == 'x':
+            while PROCESSES:
+                PROCESSES.pop().kill()
+
+
+if __name__ == '__main__':
+    main()
